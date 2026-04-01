9ebfb2b8b8b307f4eae92b711c1e35709ba66271
Keven <kevendicamargo@gmail.com>
2026-04-01 16:15:42 -0300

    chore: remove unused CSS files and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import MainRoutes from "./routes";
-import './App.css'
 
 function App() {
 
